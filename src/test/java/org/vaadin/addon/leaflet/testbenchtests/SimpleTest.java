e0c030259abccad64dc8fd85015d6f199ee6cdc9
Matti Tahvonen <matti@vaadin.com>
2013-12-05 16:45:20 +0200

    Improved automatic integration test
    Now all test UIs are opened with debug mode and failed if errors are displayed.

--- src/test/java/org/vaadin/addon/leaflet/testbenchtests/SimpleTest.java
+++ src/test/java/org/vaadin/addon/leaflet/testbenchtests/SimpleTest.java
@@ -3,20 +3,38 @@ package org.vaadin.addon.leaflet.testbenchtests;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.vaadin.addon.leaflet.demoandtestapp.util.TListUi;
+
+import com.vaadin.data.Container;
 
 public class SimpleTest extends AbstractTestBenchTest {
 
     @Test
-    public void basic() throws IOException, AssertionError {
+    public void checkAllTestsOpenWithoutErrors() throws IOException, AssertionError {
         startBrowser();
 
         driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
-
-        driver.get(BASEURL + "BasicTest");
         
-        testBench.resizeViewPortTo(640, 400);
-
+        Container listTestClasses = TListUi.listTestClasses();
+        for (Object id : listTestClasses.getItemIds()) {
+			String name = (String) listTestClasses.getItem(id)
+					.getItemProperty("name").getValue();
+			
+			driver.get(BASEURL + name + "?debug");
+			try {
+				WebElement error = driver.findElement(By.className("v-Notification-error"));
+				Assert.fail("Test " + name + " has client side exception");
+			} catch (NoSuchElementException e) {
+				continue;
+			}
+			
+		}
+        
     }
 
 }
