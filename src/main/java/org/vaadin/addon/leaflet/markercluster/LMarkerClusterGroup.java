f659395168de1c8a3abbdb924997604e268f13b2
Will Temperley <willtemperley@gmail.com>
2014-03-04 15:27:16 +0100

    Tidy up of incorrect comments and extraneous code

--- src/main/java/org/vaadin/addon/leaflet/markercluster/LMarkerClusterGroup.java
+++ src/main/java/org/vaadin/addon/leaflet/markercluster/LMarkerClusterGroup.java
@@ -5,15 +5,7 @@ import org.vaadin.addon.leaflet.LFeatureGroup;
 import com.vaadin.ui.Component;
 
 /**
- * The requirement is to have a single locations data source which can be updated, and re-used, as required.
- * 
- * This will require the ability to change or update the visualization options
- * 
- * Will need a unique, application known ID for the features
- * 
- * 
- * @author will
- *
+ * @author Will Temperley
  */
 public class LMarkerClusterGroup extends LFeatureGroup {
 	
--- src/main/java/org/vaadin/addon/leaflet/markercluster/client/LeafletMarkerClusterConnector.java
+++ src/main/java/org/vaadin/addon/leaflet/markercluster/client/LeafletMarkerClusterConnector.java
@@ -1,13 +1,10 @@
 package org.vaadin.addon.leaflet.markercluster.client;
 
-import org.peimari.gleaflet.client.ILayer;
 import org.peimari.gleaflet.client.LayerGroup;
-import org.vaadin.addon.leaflet.client.AbstractLeafletLayerConnector;
 import org.vaadin.addon.leaflet.client.LeafletFeatureGroupConnector;
 import org.vaadin.addon.leaflet.markercluster.LMarkerClusterGroup;
 import org.vaadin.gleaflet.markercluster.client.MarkerClusterGroup;
 
-import com.vaadin.client.ServerConnector;
 import com.vaadin.shared.ui.Connect;
 
 @Connect(LMarkerClusterGroup.class)
@@ -16,18 +13,6 @@ public class LeafletMarkerClusterConnector extends LeafletFeatureGroupConnector 
 	@Override
 	protected LayerGroup createLayerGroup() {
 		return MarkerClusterGroup.create();
-	}
-
-	public AbstractLeafletLayerConnector<?> getConnectorFor(ILayer iLayer) {
-		for (ServerConnector c : getChildren()) {
-			if (c instanceof AbstractLeafletLayerConnector<?>) {
-				AbstractLeafletLayerConnector<?> lc = (AbstractLeafletLayerConnector<?>) c;
-				if(lc.getLayer() == iLayer) {
-					return lc;
-				}
-			}
-		}
-		return null;
 	}
 
 }
