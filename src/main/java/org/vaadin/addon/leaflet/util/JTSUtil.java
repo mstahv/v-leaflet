a74e20629d6c41c069ac1c3697f0c1489e51cbc0
Matti Tahvonen <matti@vaadin.com>
2014-03-07 02:05:59 +0200

    LinearRing JTS type now converted to LPolygon

--- src/main/java/org/vaadin/addon/leaflet/util/JTSUtil.java
+++ src/main/java/org/vaadin/addon/leaflet/util/JTSUtil.java
@@ -31,233 +31,245 @@ import com.vividsolutions.jts.geom.PrecisionModel;
  */
 public class JTSUtil {
 
-	/**
-	 * Translates a JTS {@link Geometry} to a {@link LeafletLayer}. If given
-	 * geometry maps to multiple LeafletLayers they are returned in a
-	 * LLayerGroup.
-	 * 
-	 * @param geom
-	 *            {@link Geometry}
-	 * 
-	 * @return
-	 */
-	public static LeafletLayer toLayer(Geometry geom) {
-		Collection<LeafletLayer> layers = toLayers(geom);
-		if (layers.size() != 1) {
-			LLayerGroup group = new LLayerGroup();
-			for (LeafletLayer l : layers) {
-				group.addComponent(l);
-			}
-			return group;
-		} else {
-			return layers.iterator().next();
-		}
-	}
+    /**
+     * Translates a JTS {@link Geometry} to a {@link LeafletLayer}. If given
+     * geometry maps to multiple LeafletLayers they are returned in a
+     * LLayerGroup.
+     *
+     * @param geom {@link Geometry}
+     *
+     * @return
+     */
+    public static LeafletLayer toLayer(Geometry geom) {
+        Collection<LeafletLayer> layers = toLayers(geom);
+        if (layers.size() != 1) {
+            LLayerGroup group = new LLayerGroup();
+            for (LeafletLayer l : layers) {
+                group.addComponent(l);
+            }
+            return group;
+        } else {
+            return layers.iterator().next();
+        }
+    }
 
-	/**
-	 * Translates between a JTS {@link Geometry} and one or more
-	 * {@link LeafletLayer}s
-	 * 
-	 * @param geom
-	 *            {@link Geometry}
-	 * 
-	 * @return
-	 */
-	public static Collection<LeafletLayer> toLayers(Geometry geom) {
+    /**
+     * Translates between a JTS {@link Geometry} and one or more
+     * {@link LeafletLayer}s
+     *
+     * @param geom {@link Geometry}
+     *
+     * @return
+     */
+    public static Collection<LeafletLayer> toLayers(Geometry geom) {
 
-		Collection<LeafletLayer> layers = new HashSet<LeafletLayer>();
+        Collection<LeafletLayer> layers = new HashSet<LeafletLayer>();
 
-		if (geom instanceof Polygon) {
+        if (geom instanceof Polygon) {
+            LPolygon lPolygon = toPolygon((Polygon) geom);
+            layers.add(lPolygon);
+            return layers;
+        } else if (geom instanceof LinearRing) {
+            LPolygon lPolygon = toPolygon((LinearRing) geom);
+            layers.add(lPolygon);
+            return layers;
+        } else if (geom instanceof LineString) {
+            LPolyline lPolyline = toPolyline((LineString) geom);
+            layers.add(lPolyline);
+        } else if (geom instanceof MultiPolygon) {
+            LLayerGroup group = new LLayerGroup();
+            for (int i = 0; i < geom.getNumGeometries(); i++) {
+                Polygon polygon = (Polygon) geom.getGeometryN(i);
+                LPolygon lPolygon = toPolygon(polygon);
+                group.addComponent(lPolygon);
+            }
+            layers.add(group);
 
-			Polygon polygon = (Polygon) geom;
-			LPolygon lPolygon = toPolygon(polygon);
-			layers.add(lPolygon);
-			return layers;
+        } else if (geom instanceof MultiLineString) {
+            LLayerGroup group = new LLayerGroup();
+            for (int i = 0; i < geom.getNumGeometries(); i++) {
+                LineString lineString = (LineString) geom.getGeometryN(i);
+                LPolyline lPolyline = toPolyline(lineString);
+                group.addComponent(lPolyline);
+            }
+            layers.add(group);
 
-		} else if (geom instanceof LineString) {
+        } else if (geom instanceof com.vividsolutions.jts.geom.Point) {
 
-			LPolyline lPolyline = toPolyline((LineString) geom);
-			layers.add(lPolyline);
+            com.vividsolutions.jts.geom.Point point = (com.vividsolutions.jts.geom.Point) geom;
+            LMarker lMarker = toLMarker(point);
+            layers.add(lMarker);
 
-		} else if (geom instanceof MultiPolygon) {
-			LLayerGroup group = new LLayerGroup();
-			for (int i = 0; i < geom.getNumGeometries(); i++) {
-				Polygon polygon = (Polygon) geom.getGeometryN(i);
-				LPolygon lPolygon = toPolygon(polygon);
-				group.addComponent(lPolygon);
-			}
-			layers.add(group);
+        } else if (geom instanceof MultiPoint) {
+            LLayerGroup group = new LLayerGroup();
+            for (int i = 0; i < geom.getNumGeometries(); i++) {
+                com.vividsolutions.jts.geom.Point point = (com.vividsolutions.jts.geom.Point) geom
+                        .getGeometryN(i);
+                LMarker lMarker = toLMarker(point);
+                group.addComponent(lMarker);
+            }
+            layers.add(group);
+        }
 
-		} else if (geom instanceof MultiLineString) {
-			LLayerGroup group = new LLayerGroup();
-			for (int i = 0; i < geom.getNumGeometries(); i++) {
-				LineString lineString = (LineString) geom.getGeometryN(i);
-				LPolyline lPolyline = toPolyline(lineString);
-				group.addComponent(lPolyline);
-			}
-			layers.add(group);
+        return layers;
+    }
 
-		} else if (geom instanceof com.vividsolutions.jts.geom.Point) {
+    /**
+     * Translates between a {@link com.vividsolutions.jts.geom.Point} and a
+     * {@link LMarker}
+     *
+     * @param point
+     * @return
+     */
+    private static LMarker toLMarker(com.vividsolutions.jts.geom.Point point) {
+        LMarker lMarker = new LMarker();
+        Point lPoint = new Point();
+        lPoint.setLat(point.getY());
+        lPoint.setLon(point.getX());
+        lMarker.setPoint(lPoint);
+        return lMarker;
+    }
 
-			com.vividsolutions.jts.geom.Point point = (com.vividsolutions.jts.geom.Point) geom;
-			LMarker lMarker = toLMarker(point);
-			layers.add(lMarker);
+    /**
+     * Translates between a JTS {@link LineString} and a v-leaflet
+     * {@link Polyline}
+     *
+     * @param lineString
+     * @return
+     */
+    private static LPolyline toPolyline(LineString lineString) {
 
-		} else if (geom instanceof MultiPoint) {
-			LLayerGroup group = new LLayerGroup();
-			for (int i = 0; i < geom.getNumGeometries(); i++) {
-				com.vividsolutions.jts.geom.Point point = (com.vividsolutions.jts.geom.Point) geom
-						.getGeometryN(i);
-				LMarker lMarker = toLMarker(point);
-				group.addComponent(lMarker);
-			}
-			layers.add(group);
-		}
+        Coordinate[] coords = lineString.getCoordinates();
+        Point[] points = toPointArray(coords);
+        LPolyline polyLine = new LPolyline(points);
+        return polyLine;
+    }
 
-		return layers;
-	}
+    /**
+     * Translates between a JTS {@link Polygon} and a v-leaflet {@link LPolygon}
+     *
+     * @param polygon
+     * @return
+     */
+    public static LPolygon toPolygon(Polygon polygon) {
+        Coordinate[] coords = polygon.getBoundary().getCoordinates();
+        Point[] points = toPointArray(coords);
 
-	/**
-	 * Translates between a {@link com.vividsolutions.jts.geom.Point} and a
-	 * {@link LMarker}
-	 * 
-	 * @param point
-	 * @return
-	 */
-	private static LMarker toLMarker(com.vividsolutions.jts.geom.Point point) {
-		LMarker lMarker = new LMarker();
-		Point lPoint = new Point();
-		lPoint.setLat(point.getY());
-		lPoint.setLon(point.getX());
-		lMarker.setPoint(lPoint);
-		return lMarker;
-	}
+        LPolygon lPolygon = new LPolygon(points);
+        return lPolygon;
+    }
 
-	/**
-	 * Translates between a JTS {@link LineString} and a v-leaflet
-	 * {@link Polyline}
-	 * 
-	 * @param lineString
-	 * @return
-	 */
-	private static LPolyline toPolyline(LineString lineString) {
+    /**
+     * Translates between a JTS {@link LinearRing} and a v-leaflet
+     * {@link LPolygon}
+     *
+     * @param linearring
+     * @return
+     */
+    public static LPolygon toPolygon(LinearRing polygon) {
+        Coordinate[] coords = polygon.getCoordinates();
+        Point[] points = toPointArray(coords);
 
-		Coordinate[] coords = lineString.getCoordinates();
-		Point[] points = toPointArray(coords);
-		LPolyline polyLine = new LPolyline(points);
-		return polyLine;
-	}
+        LPolygon lPolygon = new LPolygon(points);
+        return lPolygon;
+    }
 
-	/**
-	 * Translates between a JTS {@link Polygon} and a v-leaflet {@link LPolygon}
-	 * 
-	 * @param polygon
-	 * @return
-	 */
-	public static LPolygon toPolygon(Polygon polygon) {
-		Coordinate[] coords = polygon.getBoundary().getCoordinates();
-		Point[] points = toPointArray(coords);
+    /**
+     * Translates between an array of v-leaflet {@link Point}s and JTS
+     * {@link Coordinate}s
+     *
+     * @param coords
+     * @return
+     */
+    private static Point[] toPointArray(Coordinate[] coords) {
+        Point[] points = new Point[coords.length];
 
-		LPolygon lPolygon = new LPolygon(points);
-		return lPolygon;
-	}
+        for (int i = 0; i < coords.length; i++) {
+            points[i] = new Point(coords[i].y, coords[i].x);
+        }
+        return points;
+    }
 
-	/**
-	 * Translates between an array of v-leaflet {@link Point}s and JTS
-	 * {@link Coordinate}s
-	 * 
-	 * @param coords
-	 * @return
-	 */
-	private static Point[] toPointArray(Coordinate[] coords) {
-		Point[] points = new Point[coords.length];
+    public static org.vaadin.addon.leaflet.shared.Point[] toLeafletPointArray(
+            LineString path) {
+        Coordinate[] coordinates = path.getCoordinates();
+        org.vaadin.addon.leaflet.shared.Point[] points = new org.vaadin.addon.leaflet.shared.Point[coordinates.length];
+        for (int i = 0; i < points.length; i++) {
+            Coordinate coordinate = coordinates[i];
+            points[i] = new org.vaadin.addon.leaflet.shared.Point(coordinate.y,
+                    coordinate.x);
+        }
+        return points;
+    }
 
-		for (int i = 0; i < coords.length; i++) {
-			points[i] = new Point(coords[i].y, coords[i].x);
-		}
-		return points;
-	}
+    public static LineString toLineString(
+            org.vaadin.addon.leaflet.shared.Point[] points) {
+        GeometryFactory factory = getGeometryFactory();
+        Coordinate[] coordinates = new Coordinate[points.length];
+        for (int i = 0; i < coordinates.length; i++) {
+            Point p = points[i];
+            coordinates[i] = new Coordinate(p.getLon(), p.getLat());
+        }
+        return factory.createLineString(coordinates);
+    }
 
-	public static org.vaadin.addon.leaflet.shared.Point[] toLeafletPointArray(
-			LineString path) {
-		Coordinate[] coordinates = path.getCoordinates();
-		org.vaadin.addon.leaflet.shared.Point[] points = new org.vaadin.addon.leaflet.shared.Point[coordinates.length];
-		for (int i = 0; i < points.length; i++) {
-			Coordinate coordinate = coordinates[i];
-			points[i] = new org.vaadin.addon.leaflet.shared.Point(coordinate.y,
-					coordinate.x);
-		}
-		return points;
-	}
+    public static LineString toLineString(LPolyline polyline) {
+        Point[] points = polyline.getPoints();
+        return toLineString(points);
+    }
 
-	public static LineString toLineString(
-			org.vaadin.addon.leaflet.shared.Point[] points) {
-		GeometryFactory factory = getGeometryFactory();
-		Coordinate[] coordinates = new Coordinate[points.length];
-		for (int i = 0; i < coordinates.length; i++) {
-			Point p = points[i];
-			coordinates[i] = new Coordinate(p.getLon(), p.getLat());
-		}
-		return factory.createLineString(coordinates);
-	}
+    public static LinearRing toLinearRing(LPolygon polygon) {
+        Point[] points = polygon.getPoints();
+        return toLinearRing(points);
+    }
 
-	public static LineString toLineString(LPolyline polyline) {
-		Point[] points = polyline.getPoints();
-		return toLineString(points);
-	}
+    public static Polygon toPolygon(LPolygon polygon) {
+        Point[] points = polygon.getPoints();
+        return getGeometryFactory().createPolygon(toLinearRing(points));
+    }
 
-	public static LinearRing toLinearRing(LPolygon polygon) {
-		Point[] points = polygon.getPoints();
-		return toLinearRing(points);
-	}
+    private static LinearRing toLinearRing(Point[] points) {
+        boolean closed = points[0].equals(points[points.length - 1]);
 
-	public static Polygon toPolygon(LPolygon polygon) {
-		Point[] points = polygon.getPoints();
-		return getGeometryFactory().createPolygon(toLinearRing(points));
-	}
+        Coordinate[] coordinates = new Coordinate[points.length
+                + (closed ? 0 : 1)];
+        for (int i = 0; i < points.length; i++) {
+            Point p = points[i];
+            coordinates[i] = new Coordinate(p.getLon(), p.getLat());
+        }
+        if (!closed) {
+            coordinates[coordinates.length - 1] = coordinates[0];
+        }
+        return getGeometryFactory().createLinearRing(coordinates);
+    }
 
-	private static LinearRing toLinearRing(Point[] points) {
-		boolean closed = points[0].equals(points[points.length - 1]);
+    private static GeometryFactory getGeometryFactory() {
+        GeometryFactory factory = new GeometryFactory(new PrecisionModel(),
+                4326);
+        return factory;
+    }
 
-		Coordinate[] coordinates = new Coordinate[points.length
-				+ (closed ? 0 : 1)];
-		for (int i = 0; i < points.length; i++) {
-			Point p = points[i];
-			coordinates[i] = new Coordinate(p.getLon(), p.getLat());
-		}
-		if (!closed) {
-			coordinates[coordinates.length - 1] = coordinates[0];
-		}
-		return getGeometryFactory().createLinearRing(coordinates);
-	}
+    public static com.vividsolutions.jts.geom.Point toPoint(
+            org.vaadin.addon.leaflet.shared.Point p) {
+        com.vividsolutions.jts.geom.Point point = getGeometryFactory()
+                .createPoint(new Coordinate(p.getLon(), p.getLat()));
+        return point;
+    }
 
-	private static GeometryFactory getGeometryFactory() {
-		GeometryFactory factory = new GeometryFactory(new PrecisionModel(),
-				4326);
-		return factory;
-	}
+    public static com.vividsolutions.jts.geom.Point toPoint(LMarker lMarker) {
+        return toPoint(lMarker.getPoint());
+    }
 
-	public static com.vividsolutions.jts.geom.Point toPoint(
-			org.vaadin.addon.leaflet.shared.Point p) {
-		com.vividsolutions.jts.geom.Point point = getGeometryFactory()
-				.createPoint(new Coordinate(p.getLon(), p.getLat()));
-		return point;
-	}
+    public static org.vaadin.addon.leaflet.shared.Point toLeafletPoint(
+            com.vividsolutions.jts.geom.Point location) {
+        org.vaadin.addon.leaflet.shared.Point p = new org.vaadin.addon.leaflet.shared.Point(
+                location.getY(), location.getX());
+        return p;
+    }
 
-	public static com.vividsolutions.jts.geom.Point toPoint(LMarker lMarker) {
-		return toPoint(lMarker.getPoint());
-	}
-
-	public static org.vaadin.addon.leaflet.shared.Point toLeafletPoint(
-			com.vividsolutions.jts.geom.Point location) {
-		org.vaadin.addon.leaflet.shared.Point p = new org.vaadin.addon.leaflet.shared.Point(
-				location.getY(), location.getX());
-		return p;
-	}
-
-	public static Bounds getBounds(Geometry geometry) {
-		Geometry envelope = geometry.getEnvelope();
-		return new Bounds(toPointArray(envelope.getCoordinates()));
-	}
+    public static Bounds getBounds(Geometry geometry) {
+        Geometry envelope = geometry.getEnvelope();
+        return new Bounds(toPointArray(envelope.getCoordinates()));
+    }
 
 }
