45a14ca93d5e190ed759951ba0b54d28062700f7
octavm <octavm@startmail.com>
2015-10-01 12:36:04 +0200

    Create ContextMenuServerRpc.java

---
+++ src/main/java/org/vaadin/addon/leaflet/shared/ContextMenuServerRpc.java
@@ -0,0 +1,10 @@
+package org.vaadin.addon.leaflet.shared;
+
+
+import com.vaadin.shared.communication.ServerRpc;
+
+public interface ContextMenuServerRpc extends ServerRpc {
+
+    void onContextMenu(Point p);
+
+}
