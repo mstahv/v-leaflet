ed07bd8bd5a00f0976d857d6f037484f46e7e620
Matti Tahvonen <matti@vaadin.com>
2016-11-11 16:21:27 +0200

    Added example how to use SVG with markers

--- src/test/java/org/vaadin/addon/leaflet/demoandtestapp/FontawesomeMarker.java
+++ src/test/java/org/vaadin/addon/leaflet/demoandtestapp/FontawesomeMarker.java
@@ -6,9 +6,12 @@ import org.vaadin.addon.leaflet.LMap;
 import org.vaadin.addon.leaflet.LMarker;
 
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
 import org.vaadin.addon.leaflet.LCircleMarker;
 import org.vaadin.addon.leaflet.LOpenStreetMapLayer;
+import org.vaadin.addon.leaflet.LeafletClickEvent;
+import org.vaadin.addon.leaflet.LeafletClickListener;
 import org.vaadin.addon.leaflet.shared.Point;
 import org.vaadin.addonhelpers.AbstractTest;
 
@@ -30,6 +33,7 @@ public class FontawesomeMarker extends AbstractTest {
         // Getting my map.
         LMap map = new LMap();
         map.addComponent(new LOpenStreetMapLayer());
+
         LMarker lMarker = new LMarker(61, 22);
         lMarker.setIcon(FontAwesome.BEER);
         map.addComponent(lMarker);
@@ -37,6 +41,26 @@ public class FontawesomeMarker extends AbstractTest {
         LCircleMarker lCircleMarker = new LCircleMarker(61,22, 2);
         map.addComponent(lCircleMarker);
         
+
+        LMarker lMarker2 = new LMarker(61, 23);
+        String svgCode = "<svg width=\"100\" height=\"100\">\n" +
+"  <circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"green\" stroke-width=\"4\" fill=\"yellow\" />\n" +
+"</svg>";
+        lMarker2.setIconSize(new Point(100, 100));
+        lMarker2.setIconAnchor(new Point(50, 50));
+        lMarker2.addStyleName("v-leaflet-custom-svg");
+        lMarker2.setDivIcon(svgCode);
+        lMarker2.addClickListener(new LeafletClickListener() {
+            @Override
+            public void onClick(LeafletClickEvent event) {
+                Notification.show("Clicked " +  event.getPoint().toString());
+            }
+        });
+        map.addComponent(lMarker2);
+        
+        LCircleMarker lCircleMarker2 = new LCircleMarker(61,23, 2);
+        map.addComponent(lCircleMarker2);
+
         map.zoomToContent();
 
         layout.addComponent(map);
