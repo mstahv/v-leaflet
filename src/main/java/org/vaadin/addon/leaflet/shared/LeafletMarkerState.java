60a7698d519931bcf912517d4444104f61748298
Krassimir Valev <krassimir_valev@yahoo.com>
2017-08-18 18:26:23 +0200

    Basic FontAwesome marker customization

--- src/main/java/org/vaadin/addon/leaflet/shared/LeafletMarkerState.java
+++ src/main/java/org/vaadin/addon/leaflet/shared/LeafletMarkerState.java
@@ -1,19 +1,21 @@
 package org.vaadin.addon.leaflet.shared;
 
-
 public class LeafletMarkerState extends AbstractLeafletComponentState {
 
-	public Point point;
-	public Point iconSize;
-	public Point iconAnchor;
-	public Point popupAnchor;
-	public String divIcon;
-	public String title;
-	public String tooltip;
-	public TooltipState tooltipState;
-	public String popup;
-	public PopupState popupState;
-	public Boolean draggable;
+    public Point point;
+    public Point iconSize;
+    public Point iconAnchor;
+    public String iconPathFill;
+    public String iconPathStroke;
+    public String iconTextFill;
+    public Point popupAnchor;
+    public String divIcon;
+    public String title;
+    public String tooltip;
+    public TooltipState tooltipState;
+    public String popup;
+    public PopupState popupState;
+    public Boolean draggable;
     public Integer zIndexOffset;
 
 }
