938908c0157d929ddce91c007c60d9043c6ca45f
Matti Tahvonen <matti@vaadin.com>
2017-09-21 13:49:36 +0300

    run tests using Chrome as FirefoxDriver is broken and don't want to update this branch to use the gecko driver et all

--- src/test/java/org/vaadin/addon/leaflet/testbenchtests/SimpleTest.java
+++ src/test/java/org/vaadin/addon/leaflet/testbenchtests/SimpleTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.vaadin.addonhelpers.TListUi;
 import org.vaadin.addonhelpers.automated.AbstractWebDriverCase;
 
@@ -39,4 +41,8 @@ public class SimpleTest extends AbstractWebDriverCase {
         
     }
 
+	@Override
+	protected void startBrowser() {
+		startBrowser(new ChromeDriver());
+	}
 }
