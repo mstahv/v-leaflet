50d8d6d64623e92a327822bc7dc54646de30f325
octavm <octavm@startmail.com>
2018-07-20 13:36:06 +0200

    Added mouse events support for LImageOverlay. Fixes #184

--- src/main/java/org/vaadin/addon/leaflet/shared/LeafletImageOverlayState.java
+++ src/main/java/org/vaadin/addon/leaflet/shared/LeafletImageOverlayState.java
@@ -1,8 +1,12 @@
 package org.vaadin.addon.leaflet.shared;
 
 public class LeafletImageOverlayState extends AbstractLeafletVectorState {
+
     public Bounds bounds;
     public Double opacity;
+    public String alt;
+    public Boolean interactive;
+    public Integer zIndex;
     public String attribution;
     
 }
