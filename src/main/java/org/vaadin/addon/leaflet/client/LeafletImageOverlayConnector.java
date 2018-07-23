16d1c5d01c1bb912accb2b46762fa315ee466dc2
octavm <octavm@startmail.com>
2018-07-23 10:28:15 +0200

    Added mouse events support for LImageOverlay. Fixes #184

--- src/main/java/org/vaadin/addon/leaflet/client/LeafletImageOverlayConnector.java
+++ src/main/java/org/vaadin/addon/leaflet/client/LeafletImageOverlayConnector.java
@@ -1,18 +1,13 @@
 package org.vaadin.addon.leaflet.client;
 
+import com.google.gwt.core.client.Scheduler;
 import com.vaadin.client.MouseEventDetailsBuilder;
+import org.peimari.gleaflet.client.*;
 import org.vaadin.addon.leaflet.shared.DragEndServerRpc;
-
-import org.peimari.gleaflet.client.ClickListener;
-import org.peimari.gleaflet.client.LatLng;
-import org.peimari.gleaflet.client.MouseEvent;
 
 import com.vaadin.shared.communication.URLReference;
 import com.vaadin.shared.ui.Connect;
-import org.peimari.gleaflet.client.ImageOverlay;
-import org.peimari.gleaflet.client.ImageOverlayOptions;
-import org.peimari.gleaflet.client.LatLngBounds;
-import org.peimari.gleaflet.client.Layer;
+import org.vaadin.addon.leaflet.shared.EventId;
 import org.vaadin.addon.leaflet.shared.LeafletImageOverlayState;
 
 @Connect(org.vaadin.addon.leaflet.LImageOverlay.class)
@@ -40,6 +35,10 @@ public class LeafletImageOverlayConnector extends
 	protected void update() {
 		if (imageOverlay != null) {
 			removeLayerFromParent();
+			imageOverlay.removeClickListener();
+			imageOverlay.removeContextMenuListener();
+			imageOverlay.removeMouseOverListener();
+			imageOverlay.removeMouseOutListener();
 		}
         
         LatLngBounds bounds = LatLngBounds.create(LatLng.create(getState().bounds.getSouthWestLat(), 
@@ -56,11 +55,57 @@ public class LeafletImageOverlayConnector extends
         if(getState().opacity != null) {
             options.setOpacity(getState().opacity);
         }
-                
+        if (getState().alt != null) {
+			options.setAlt(getState().alt);
+		}
+		if (getState().interactive != null) {
+			options.setInteractive(getState().interactive);
+		}
+		if (getState().zIndex != null) {
+			options.setZIndex(getState().zIndex);
+		}
+
         String url = getConnection().translateVaadinUri(urlReference.getURL());
 		imageOverlay = ImageOverlay.create(url, bounds, options);
 		addToParent(imageOverlay);
-        
+
+		imageOverlay.addClickListener(handler);
+		if (hasEventListener(EventId.CONTEXTMENU)) {
+			imageOverlay.addContextMenuListener(new ContextMenuListener() {
+				@Override
+				public void onContextMenu(MouseEvent event) {
+					contextMenuRpc.onContextMenu(U.toPoint(event.getLatLng()),
+							MouseEventDetailsBuilder.buildMouseEventDetails(event.getNativeEvent(), getLeafletMapConnector().getWidget().getElement()));
+				}
+			});
+		}
+		if (hasEventListener(EventId.MOUSEOVER)) {
+            /*
+             * Add listener lazily to avoid extra event if layer is modified in
+             * server side listener. This can be removed if "clear and rebuild"
+             * style component updates are changed into something more
+             * intelligent at some point.
+             */
+			Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+				@Override
+				public void execute() {
+					imageOverlay.addMouseOverListener(new MouseOverListener() {
+						@Override
+						public void onMouseOver(MouseEvent event) {
+							mouseOverRpc.onMouseOver(U.toPoint(event.getLatLng()));
+						}
+					});
+				}
+			});
+		}
+		if (hasEventListener(EventId.MOUSEOUT)) {
+			imageOverlay.addMouseOutListener(new MouseOutListener() {
+				@Override
+				public void onMouseOut(MouseEvent event) {
+					mouseOutRpc.onMouseOut(U.toPoint(event.getLatLng()));
+				}
+			});
+		}
 	}
 
 	@Override
